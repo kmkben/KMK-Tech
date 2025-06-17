d7752a3c44d41752a4b12a6e234c352637c36718
Kemoko KEITA <kemokokeita710@gmail.com>
2025-06-17 13:10:43 +0100

    Setting up the project and add tailwind as style css

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    
+    template_name = 'home/home.html'
+    
+    context = {
+        
+    }
+    return render(request, template_name, context)
