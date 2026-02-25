08cc63a6701b39a6aba8a756d3335b3206d669bf
Kemoko KEITA <kemokokeita710@gmail.com>
2026-02-25 15:42:31 +0000

    Adding contactemail send and .env for environnement vars

--- KMK_Tech/settings.py
+++ KMK_Tech/settings.py
@@ -10,23 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
+import os
 from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-#6^bx$2wx_$zh30(rov8ad4+f66o@=s*1(b6*kyc%riu4!!g)o'
-SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = env.bool("DEBUG", default=False)
+
+SECRET_KEY = env("SECRET_KEY")
+
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 ALLOWED_HOSTS = []
 
@@ -148,3 +150,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = ["127.0.0.1"]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = f"KMK IOTech Contact <{EMAIL_HOST_USER}>"
--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import ContactForm
 
+from django.core.mail import EmailMessage
+from django.conf import settings
+
 # Create your views here.
 
 def contact(request):
@@ -14,6 +17,28 @@ def contact(request):
             email = form.cleaned_data['email']
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
+            
+            email_subject = f"KMK IOTech Contact — {subject}"
+            
+            email_body = f"""
+New message KMK IOTech from :
+
+Name: {name}
+Email: {email}
+
+Message: 
+{message}
+"""
+            
+            email_message = EmailMessage(
+                subject=email_subject,
+                body=email_body,
+                from_email='kemokokeita710@gmail.com', #settings.DEFAULT_FROM_EMAIL
+                to=["kemoko.keita@outlook.com"],  # ← YOUR OUTLOOK
+                reply_to=[email],  # VERY IMPORTANT ⭐
+            )
+
+            email_message.send(fail_silently=False)
 
             # Here we use Django messages framework to show success
             messages.success(request, "✅ Your message has been sent successfully!")
